549535cca3fd2d8060a4b7373494e8b5858d6227
Vimal Bhalodia <vimalb@users.noreply.github.com>
2026-03-15 22:42:52 -0400

    Reenable entitlement management

--- hooks/afterPrepareHook.js
+++ hooks/afterPrepareHook.js
@@ -76,10 +76,10 @@ function activateUniversalLinksInAndroid(cordovaContext, pluginPreferences) {
  */
 function activateUniversalLinksInIos(cordovaContext, pluginPreferences) {
   // modify xcode project preferences
- // iosProjectPreferences.enableAssociativeDomainsCapability(cordovaContext);
+  iosProjectPreferences.enableAssociativeDomainsCapability(cordovaContext);
 
   // generate entitlements file
-//  iosProjectEntitlements.generateAssociatedDomainsEntitlements(cordovaContext, pluginPreferences);
+  iosProjectEntitlements.generateAssociatedDomainsEntitlements(cordovaContext, pluginPreferences);
 
   // generate apple-site-association-file
   iosAppSiteAssociationFile.generate(cordovaContext, pluginPreferences);
